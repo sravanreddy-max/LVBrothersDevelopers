7986391d1b73f25236c3bd8b1326a7125c08dcbd
sravan321-hub <lekkalasrav1143@gmail.com>
2025-10-27 19:11:37 +0530

    hash

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import {  HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { I18nProvider } from './i18n'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <I18nProvider>
         <App />
       </I18nProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
